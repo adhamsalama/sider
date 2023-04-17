67f3e28301e523916ae75532128e1c4b20d267d4
Adham Salama <adham.salama@wuilt.com>
2023-04-17 14:58:58 +0200

    Enhance error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use sider::Sider;
-use std::{env, time::Duration};
+use std::{env, process::exit, time::Duration};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,21 +13,28 @@ fn parse_args(args: Vec<String>) -> (u32, Option<Duration>) {
     let mut timeout: Option<Duration> = None;
     for (index, arg) in args.iter().enumerate() {
         if arg == "-p" {
-            port = args[index + 1]
-                .clone()
-                .parse()
-                .expect("Port should be an integer");
+            let parsed_port = args[index + 1].clone().parse();
+            match parsed_port {
+                Ok(p) => port = p,
+                Err(_) => {
+                    println!("Error: Port should be an integer");
+                    exit(1);
+                }
+            }
             if port < 1 || port > 65535 {
-                panic!("Port should be between 1 and 65535");
+                println!("Error: Port should be between 1 and 65535");
+                exit(1);
             }
         }
         if arg == "-t" {
-            timeout = Some(Duration::from_millis(
-                args[index + 1]
-                    .clone()
-                    .parse()
-                    .expect("Timeout should be an integer"),
-            ));
+            let parsed_timeout = args[index + 1].clone().parse();
+            match parsed_timeout {
+                Ok(t) => timeout = Some(Duration::from_millis(t)),
+                Err(_) => {
+                    println!("Error: Timeout should be an integer");
+                    exit(1);
+                }
+            }
         }
     }
     (port, timeout)
